430ab81abe0be4e87200161f5c7d45c293524226
Nhan Huynh Thien <nhanhuynh7115@gmail.com>
2025-10-09 12:12:46 +0700

    Update Response Total Fee

--- src/main/java/com/evdealer/evdealermanagement/controller/admin/AdminProductManagementController.java
+++ src/main/java/com/evdealer/evdealermanagement/controller/admin/AdminProductManagementController.java
@@ -5,6 +5,7 @@ import com.evdealer.evdealermanagement.dto.product.detail.ProductDetail;
 import com.evdealer.evdealermanagement.exceptions.AppException;
 import com.evdealer.evdealermanagement.exceptions.ErrorCode;
 import com.evdealer.evdealermanagement.service.implement.AdminService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,19 +26,20 @@ public class AdminProductManagementController {
 
     @GetMapping("/all")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ApiResponse<List<ProductDetail>> getAllProducts() {
+    public ResponseEntity<List<ProductDetail>> getAllProducts() {
         List<ProductDetail> products = adminService.getAllProducts();
-        return new ApiResponse<>(200, "Fetched all products", products);
+        return ResponseEntity.ok().body(products);
     }
 
     @GetMapping("/all-posting-fee")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ApiResponse<String> getAllPostingFee() {
-       try {
-           String totalPostingFee = adminService.getTotalFee();
-           return new ApiResponse<>(200, "Total Fee", totalPostingFee);
-       } catch (Exception e) {
-           throw new AppException(ErrorCode.BAD_REQUEST);
-       }
+    public ResponseEntity<String> getAllPostingFee() {
+        try {
+            String totalPostingFee = adminService.getTotalFee();
+            return ResponseEntity.ok().body("totalFee: " + totalPostingFee);
+        } catch (Exception e) {
+            throw new AppException(ErrorCode.BAD_REQUEST);
+        }
     }
+
 }
