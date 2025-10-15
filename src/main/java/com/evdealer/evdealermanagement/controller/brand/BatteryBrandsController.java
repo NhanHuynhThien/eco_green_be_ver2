516f80d2de0dd0e4a9472a06c1eb92f1d405f35f
Nhan Lee <lechinhan18012005@gmail.com>
2025-10-15 23:10:11 +0700

    update API Show Brands

--- src/main/java/com/evdealer/evdealermanagement/controller/brand/BatteryBrandsController.java
+++ src/main/java/com/evdealer/evdealermanagement/controller/brand/BatteryBrandsController.java
@@ -3,11 +3,6 @@ package com.evdealer.evdealermanagement.controller.brand;
 import com.evdealer.evdealermanagement.dto.battery.brand.BatteryBrandsResponse;
 import com.evdealer.evdealermanagement.service.implement.BatteryService;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-
-import org.springframework.security.access.prepost.PreAuthorize;
-=======
->>>>>>> 4ffbfab9eadc44b78aa97688bc8da1e1c676bc10
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +17,6 @@ public class BatteryBrandsController {
     private final BatteryService batteryService;
 
     @GetMapping("/show")
-<<<<<<< HEAD
-    @PreAuthorize("hasRole('STAFF') or hasRole('ADMIN')")
-=======
->>>>>>> 4ffbfab9eadc44b78aa97688bc8da1e1c676bc10
     public List<BatteryBrandsResponse> getAllBrandsLogoName() {
         return batteryService.listAllBatteryNameAndLogo();
     }
--- src/main/java/com/evdealer/evdealermanagement/controller/brand/VehicleBrandsController.java
+++ src/main/java/com/evdealer/evdealermanagement/controller/brand/VehicleBrandsController.java
@@ -4,10 +4,6 @@ import com.evdealer.evdealermanagement.dto.vehicle.brand.VehicleBrandsResponse;
 import com.evdealer.evdealermanagement.service.implement.VehicleService;
 
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-
-=======
->>>>>>> 4ffbfab9eadc44b78aa97688bc8da1e1c676bc10
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +17,7 @@ public class VehicleBrandsController {
 
     private final VehicleService vehicleService;
 
-<<<<<<< HEAD
-    @GetMapping("/brands/show")
-=======
     @GetMapping("/show")
->>>>>>> 4ffbfab9eadc44b78aa97688bc8da1e1c676bc10
     public List<VehicleBrandsResponse> getAllBrandsLogoName() {
         return vehicleService.listAllVehicleNameAndLogo();
     }
