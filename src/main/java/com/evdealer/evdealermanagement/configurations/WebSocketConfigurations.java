d1335fec5ed2d3985919730aaf72e7f0cbc1c61c
Nhan Huynh Thien <nhanhuynh7115@gmail.com>
2025-10-16 11:07:25 +0700

    Fix Dockerfile for Railway build

--- src/main/java/com/evdealer/evdealermanagement/configurations/WebSocketConfigurations.java
+++ src/main/java/com/evdealer/evdealermanagement/configurations/WebSocketConfigurations.java
@@ -1,4 +1,20 @@
 package com.evdealer.evdealermanagement.configurations;
 
-public class WebSocketConfigurations {
+import com.evdealer.evdealermanagement.service.implement.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@Slf4j
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfigurations implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtService jwtService;
+
+
 }
