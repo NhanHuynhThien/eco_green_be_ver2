6c5826ca848809508af17562e012796ef94b92d0
Nhan Lee <lechinhan18012005@gmail.com>
2025-10-22 07:53:34 +0700

    add VehicleCatalogRepository function

--- src/main/java/com/evdealer/evdealermanagement/repository/VehicleCatalogRepository.java
+++ src/main/java/com/evdealer/evdealermanagement/repository/VehicleCatalogRepository.java
@@ -9,14 +9,11 @@ import com.evdealer.evdealermanagement.entity.vehicle.VehicleCatalog;
 public interface VehicleCatalogRepository extends JpaRepository<VehicleCatalog, String> {
     Optional<VehicleCatalog> findByVersionId(String versionId);
 
-<<<<<<< Updated upstream
     Optional<VehicleCatalog> findByVersion_Id(String versionId);
 
-=======
     Optional<VehicleCatalog> findByVersionIdAndBrandIdAndModelAndYear(
             String versionId,
             String brandId,
             String model,
             Short year);
->>>>>>> Stashed changes
 }
