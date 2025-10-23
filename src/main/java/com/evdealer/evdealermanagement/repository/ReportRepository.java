a372c1c44e750d0bf39aef3b93d127dab159bf46
lequochoi1910 <lequochoi7390@gmail.com>
2025-10-23 22:21:36 +0700

    Create service and repo for report

--- src/main/java/com/evdealer/evdealermanagement/repository/ReportRepository.java
+++ src/main/java/com/evdealer/evdealermanagement/repository/ReportRepository.java
@@ -1,4 +1,7 @@
 package com.evdealer.evdealermanagement.repository;
 
-//public interface ReportRepository extends Jpa{
-//}
+import com.evdealer.evdealermanagement.entity.report.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report,String> {
+}
