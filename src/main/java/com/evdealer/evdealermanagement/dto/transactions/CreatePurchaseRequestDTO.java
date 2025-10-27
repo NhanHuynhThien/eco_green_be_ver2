90e6c1003b78b92b004739d8f843e9964ee7c0fb
Nhan Huynh Thien <nhanhuynh7115@gmail.com>
2025-10-27 12:35:34 +0700

    Fix Conflict CreatePurchaseDto

--- src/main/java/com/evdealer/evdealermanagement/dto/transactions/CreatePurchaseRequestDTO.java
+++ src/main/java/com/evdealer/evdealermanagement/dto/transactions/CreatePurchaseRequestDTO.java
@@ -4,10 +4,7 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -15,6 +12,7 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class CreatePurchaseRequestDTO {
 
     @NotBlank(message = "Product ID is required")
